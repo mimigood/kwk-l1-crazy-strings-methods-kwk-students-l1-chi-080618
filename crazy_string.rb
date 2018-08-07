d8294c5290df43ddf410ef28b01b40682f9638b0
mimigood <mbgss07@gmail.com>
2018-08-07 17:16:40 +0000

    Done.

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,8 @@
 # write your method here
 
-def crazy_strings(a,b)
+def crazy_strings("a","b")
+  a=a.reverse.upcase
+  b=b.gsub("s" , "z").gsub("S", "Z").swapcase
 
 end
+crazy_strings("hello", "friends")
